9df93e33b957a929cfa4f68f8b5eb615a395eefc
吴豪(wuhao01) <wuhao01@51talk.com>
2017-11-21 17:36:45 +0800

    gulp-jst-extend2

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # gulp-jst-extend2
-Using lodash tempate to make the template modular management, each module is synthesized into a CMD JS, convenient for calling and management. This plug-in solves the problem of picture CDN cache, as long as the modified template, the path behind the picture will automatically compile and add version number
+Using lodash tempate to make the template modular management, each module is synthesized into a CMD or AMD JS, convenient for calling and management. This plug-in solves the problem of picture CDN cache, as long as the modified template, the path behind the picture will automatically compile and add version number
+
+# gulp-jst-extend
+
+> gulp plugin to compile underscore / lodash templates to js file(s).
+
+Inspired by module [gulp-jst-concat](https://github.com/mozuo/gulp-jst-extend2.git) but this plugin add cmd/amd support, and will add more features soon.
+
+## Install
+Install using [npm](https://github.com/mozuo/gulp-jst-extend2.git).
+
+    $ npm install gulp-jst-extend2
+
+
+## Usage
+
+``` js
+gulp.task('jst', function(project, type, year) {
+    if (!year) {
+        year = new Date().getFullYear();
+    }
+
+    var jst = require('gulp-jst-extend');
+    console.log("templatePath:" + templatePath)
+    return gulp.src([type + '/project/' + year + '/' + project + '/source/tpl/**/*.html'])
+        .pipe(changed(type + '/project/' + year + '/' + project + '/source/tpl/' + templatePath + '/', { extension: '.js' }))
+        .pipe(jst('index.js', { renameKeys: ['^.*\/tpl\/.*\/(.*).html$', '$1'] }))
+        .pipe(gulp.dest(type + '/project/' + year + '/' + project + '/source/js/tpl/' + templatePath + "/"))
+        .on("end", function() {
+            gulp.src(type + '/project/' + year + '/' + project + '/source/js/tpl/' + templatePath + '/*.js')
+                .pipe(uglify({
+                    output: {
+                        ascii_only: true
+                    }
+                }))
+                .pipe(gulp.dest(type + '/project/' + year + '/' + project + '/js/tpl/' + templatePath + "/"))
+        })
+});
+```
+
+More details please goto [gulp-jst-concat](https://github.com/tambourinecoder/gulp-jst-concat).
+
+## License
+MIT
+
---
+++ index.js
@@ -0,0 +1,97 @@
+"use strict";
+const jsdom = require("jsdom");
+
+require('jsdom-global')()
+
+const { JSDOM } = jsdom;
+var gUtil = require('gulp-util'),
+    PluginError = gUtil.PluginError,
+    File = gUtil.File,
+    through = require('through'),
+    _ = require('lodash'),
+    printf = require('util').format;
+
+var outputTmpl = ";(function(root, factory) {\
+  if (typeof module === 'object' && module.exports) module.exports = factory();\
+  else if (typeof define === 'function') define(factory);\
+  else root = factory();\
+}(typeof window === 'object' ? window : this, function() {\
+  return {%s}\
+}));";
+
+
+function pluginError(message) {
+    return new PluginError('gulp-jst-extend', message)
+}
+
+function compile(file, renameKeys) {
+    var path = file.path.replace(/\\/g, '/');
+    var name = path.replace(new RegExp(renameKeys[0]), renameKeys[1]),
+        contents = String(file.contents)
+    document.body.innerHTML = contents
+    const $ = require("jquery")
+    if ($(document.body).find("img").length > 0) {
+        $(document.body).find("img").each(function(i, item) {
+                var src = $(this).attr("src")
+                if (src.indexOf() > -1) {
+                    src = src.match(/^<%=(.*)%>*/)[1] + "?v=" + new Date().getTime()
+                } else {
+                    src = src + "?v=" + new Date().getTime()
+                }
+                $(this).attr("src", src)
+                console.log($(this).attr("src"))
+            })
+            // var src = $(document.body).find("img").eq(0).attr("src")
+            // $(document.body).find("img").eq(0).attr("src", src + "?v=" + new Date().getTime())
+            // console.log($(document.body).find("img").eq(0).attr("src"))
+
+    }
+    contents = String(document.body.innerHTML)
+
+    // console.log($("<img src='aaa.jpg'>").attr("src"))
+    //console.log(dom.window.document.querySelector("p"))
+
+    return {
+        name: name,
+        fnSource: _.template(contents).source
+    }
+}
+
+function buildJSTString(files, renameKeys) {
+    function compileAndRender(file) {
+        var template = compile(file, renameKeys)
+        return printf('"%s": %s', template.name, template.fnSource)
+    }
+
+    return printf(outputTmpl, files.map(compileAndRender).join(',\n'))
+}
+
+module.exports = function jstConcat(fileName, _opts) {
+    if (!fileName) throw pluginError('Missing fileName')
+
+    var defaults = { renameKeys: ['.*', '$&'] },
+        opts = _.extend({}, defaults, _opts),
+        files = []
+
+    function write(file) {
+        /* jshint validthis: true */
+        if (file.isNull()) return
+        if (file.isStream()) return this.emit('error', pluginError('Streaming not supported'))
+
+        files.push(file)
+    }
+
+    function end() {
+        /* jshint validthis: true */
+        var jstString = buildJSTString(files, opts.renameKeys)
+
+        this.queue(new File({
+            path: fileName,
+            contents: new Buffer(jstString)
+        }))
+
+        this.queue(null)
+    }
+
+    return through(write, end)
+}
